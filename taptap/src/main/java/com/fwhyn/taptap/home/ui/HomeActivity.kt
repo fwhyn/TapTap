4ffc3751c1c77495de7a92f515c7fded28029147
fwhyn <fwhynseventh@gmail.com>
2021-09-11 14:14:21 +0700

    #2 Add Home Screen

--- taptap/src/main/java/com/fwhyn/taptap/home/ui/HomeActivity.kt
+++ taptap/src/main/java/com/fwhyn/taptap/home/ui/HomeActivity.kt
@@ -1,11 +1,12 @@
-package com.fwhyn.taptap
+package com.fwhyn.taptap.home.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.fwhyn.taptap.R
 
-class MainActivity : AppCompatActivity() {
+class HomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_home)
     }
 }
